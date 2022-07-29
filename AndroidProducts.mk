3b97aa981b0123ec718afd149f8784fcdf57e3dc
Lowxorx <lowxorx@lahan.fr>
2022-07-29 17:08:09 +0200

    lmi: Spark bringup  - Add overlays for supported features  - Add flags for supported features

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,9 +5,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_lmi.mk
+    $(LOCAL_DIR)/spark_lmi.mk
 
 COMMON_LUNCH_CHOICES := \
-    lineage_lmi-user \
-    lineage_lmi-userdebug \
-    lineage_lmi-eng
+    spark_lmi-user \
+    spark_lmi-userdebug \
+    spark_lmi-eng
--- BoardConfig.mk
+++ BoardConfig.mk
@@ -26,6 +26,7 @@ TARGET_OTA_ASSERT_DEVICE := lmi,lmipro
 
 # Properties
 TARGET_VENDOR_PROP += $(DEVICE_PATH)/vendor.prop
+TARGET_SYSTEM_PROP += $(DEVICE_PATH)/system.prop
 
 # Inherit from the proprietary version
 include vendor/xiaomi/lmi/BoardConfigVendor.mk
